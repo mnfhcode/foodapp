5e9ccab46df1e580770477afae68b8b0535eebfa
Chris <monkeypy324@gmail.com>
2016-07-30 17:55:47 -0600

    Nathan is a jerk

---
+++ foodOverdoge/config/config.js
@@ -0,0 +1,13 @@
+var yelpKey = 'LJGePWj7FaGS3p0mLceB_A';
+var yelpSecret = 'agJkWjrlk2Vth5zAuODz8E14vhY';
+var yelpToken = 'q83DaArTDgrZGL0Jn-O31FQS7toCg9QQ';
+var yelpTokenSecret = 'EcRhICVxYE0wTCZU5Pxu9CfO_DI';
+
+exports.googleKey = 'AIzaSyCIjbJQc5PdnxtHlQRmM0Wc5TU-OLgVaf4';
+
+exports.yelpAPI = {
+	consumer_key: yelpKey,
+	consumer_secret: yelpSecret,
+	token: yelpToken,
+	token_secret: yelpTokenSecret,
+}
